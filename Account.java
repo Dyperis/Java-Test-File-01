d7144c70a88d89bdea49fa42eb17706d8fd6efd2
Dyperis <150345822+Dyperis@users.noreply.github.com>
2024-11-05 15:51:16 +0700

    Add files via upload
    Extra stuff removed cus it was done on a diff computer =))

---
+++ Account.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Account {
+    String name;
+    String account_number;
+    String password;
+}
+/* try {
+// Specify the file path
+File myObj = new File("C:\\Users\\Admin\\Documents\\code n stuff\\user input.txt");
+Scanner myReader = new Scanner(myObj);
+
+// Read and print each line from the file
+            while (myReader.hasNextLine()) {
+String data = myReader.nextLine();
+                System.out.println(data);
+            }
+                    myReader.close();
+        } catch (
+FileNotFoundException e) {
+        System.out.println("An error occurred.");
+            e.printStackTrace();
+        } */
