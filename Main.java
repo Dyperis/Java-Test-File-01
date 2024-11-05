df79c3e5bd18549fe679c746cc63e1a00b05f342
Dyperis <150345822+Dyperis@users.noreply.github.com>
2024-11-05 15:45:49 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+  /*     try {
+            File myObj = new File("C:\\Users\\Admin\\Documents\\code n stuff\\user input.txt");
+            Scanner myReader = new Scanner(myObj);
+            String data = myReader.nextLine();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+        Scanner Input1 = new Scanner(System.in);
+        String Password_Entered = Input1.nextLine();
+    if (Password_Entered.equals(Password_Entered)) {
+        System.out.println("Your password is correct");
+    }
+    else {
+        System.out.println("Your password is incorrect");
+    }
+
+        File myObj = new File("C:\\Users\\Admin\\Documents\\code n stuff\\user input.txt");
+        Scanner Input2 = new Scanner(System.in);
+        String Password = Input2.nextLine();
+        try {
+
+            FileWriter myWriter = new FileWriter("C:\\Users\\Admin\\Documents\\code n stuff\\user input.txt");
+            myWriter.write(Password);
+            myWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        */
+    }
+}
+
+
+
